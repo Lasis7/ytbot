f5fb484fb43cb65e124e80edece5d789e5887b46
Lasis7 <AE8656@student.jamk.fi>
2026-02-20 00:11:00 +0200

    added intents for the bot

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
-import { Client } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import 'dotenv/config';
+
+// Client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildPresences,
+  ],
+});
+
+// Run when client is ready
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
