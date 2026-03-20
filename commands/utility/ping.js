a3b945cae710fb41b2fc8838b547207782aa02f7
Lasis7 <AE8656@student.jamk.fi>
2026-03-20 17:56:15 +0200

    ping also provides delay now

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,12 @@
 import { SlashCommandBuilder } from 'discord.js';
+import client from '../../bot.js';
 
 export default {
+  cooldown: 5,
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply(`Pong! Delay is ${client.ws.ping}ms`);
   },
 };
