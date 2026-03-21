667cc58683ba9b721d63a77adeca216f6e84b6bf
Lasis7 <AE8656@student.jamk.fi>
2026-03-22 01:31:55 +0200

    added a new command for resuming the song

---
+++ commands/utility/resume.js
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('resume')
+    .setDescription('resume the paused song'),
+  async execute(interaction) {
+    const userInVc = interaction.member.voice.channel; // Guildmember in docs
+    const botInVc = interaction.guild.members.me.voice.channel; // Guild in docs
+
+    if (!userInVc) {
+      return await interaction.reply({
+        content: 'You need to be inside a voice channel to use this command',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    const connection = getVoiceConnection(interaction.member.voice.guild.id);
+
+    // If the bot is in VC but the in a different channel than the user, or there is no player active at the moment
+    if (!connection || (botInVc && userInVc.id !== botInVc.id)) {
+      return await interaction.reply({
+        content:
+          'You need to be in the same voice channel as the bot to use this command',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    connection.state.subscription.player.unpause();
+    await interaction.reply('Resuming');
+  },
+};
