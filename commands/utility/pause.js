adc9a6a50cd755856ce30ec771e0be2d6b681f79
Lasis7 <AE8656@student.jamk.fi>
2026-03-21 14:59:49 +0200

    added a command for pausing audio playback

---
+++ commands/utility/pause.js
@@ -0,0 +1,33 @@
+import { SlashCommandBuilder, MessageFlags } from 'discord.js';
+import { getVoiceConnection } from '@discordjs/voice';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('pause')
+    .setDescription('Pause the bot'),
+  async execute(interaction) {
+    const userInVc = interaction.member.voice.channel; // Guildmember in docs
+    const botInVc = interaction.guild.members.me.voice.channel; // Guild in docs
+
+    if (!userInVc) {
+      return await interaction.reply({
+        content: 'You need to be inside a voice channel to use this command',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    const connection = getVoiceConnection(interaction.member.voice.guild.id);
+
+    // If the bot is in VC but the in a different channel than the user, or there is no player active at the moment
+    if (!connection || (botInVc && userInVc.id !== botInVc.id)) {
+      return await interaction.reply({
+        content:
+          'You need to be in the same voice channel as the bot to use this command',
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    connection.state.subscription.player.pause();
+    await interaction.reply('Paused');
+  },
+};
