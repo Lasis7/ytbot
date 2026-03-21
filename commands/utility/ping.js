003244f2f53d3c3665ee96d7c3ec04ce0367ca7f
Lasis7 <AE8656@student.jamk.fi>
2026-03-21 14:58:05 +0200

    .

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,7 +5,7 @@ export default {
   cooldown: 5,
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setDescription('Replies with Pong! + delay'),
   async execute(interaction) {
     await interaction.reply(`Pong! Delay is ${client.ws.ping}ms`);
   },
